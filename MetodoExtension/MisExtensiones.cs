b711fb510ebc5808ad06b6c3eb0f53cf020d63b9
ipalma <isaac.palma@tajamar365.com>
2014-10-30 14:30:00 +0100

    ver 0.1

---
+++ MetodoExtension/MisExtensiones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoExtension
+{
+    //Los métodos de extensión solo puede ser estatico.
+    public static class MisExtensiones
+    {
+        //Esto es un metodo de extension para String, ¿cómo?, al definir los parámetros lo llamamos con this y el tipo al que se extiende.
+        public static int ContarPalabras(this String texto)
+        {
+            var pal = texto.Split(' ');
+            return pal.Length;
+        }
+
+        public static String Reemplazame(this String texto, String origen, String destino)
+        {
+            var res = texto.Replace(origen, destino);
+            return res;
+        }
+
+
+
+
+    }
+}
---
+++ MetodoExtension/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoExtension
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var nombre = "Luis Felipe Gil Lamaignere";
+
+            Console.WriteLine(nombre.ContarPalabras());
+            Console.WriteLine(nombre.Reemplazame("u","A"));
+            Console.ReadLine();
+        }
+    }
+}
